bcdf0fd0ee3bebcd39ad1cadc91c5bd9b0eee5aa
sushmitha <sushmitha21.reddy@gmail.com>
2022-07-01 21:05:09 +0530

    architect changes are done so adding

--- src/test/java/Cucumber/Automation/AppTest.java
+++ src/test/java/Cucumber/Automation/AppTest.java
@@ -2,6 +2,9 @@ package Cucumber.Automation;
 
 import org.junit.jupiter.api.Test;
 
+import cucumber.api.java.en.Given;
+import cucumber.api.java.en.When;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 public class AppTest {
@@ -9,5 +12,30 @@ public class AppTest {
   @Test
   public void shouldAnswerWithTrue() {
     assertTrue(true);
+    
+    public class StepDefinition {
+
+        @Given("^user is on landing page $")
+        public void user_is_on_landing_page() throws Throwable {
+        	System.out.println("user on landing page");
+            
+        }
+
+        @When("^user login into application by giving user name and password $")
+        public void user_login_into_application_by_giving_user_name_and_password()  {
+        	System.out.println("try to login ");
+        	System.out.println("try to login ");
+        	System.out.println("try to login ");
+        	System.out.println("try to login ");
+        	
+        	//ggffgg
+        	//ssuhsdkujskjd
+        	//ushfushf
+        	System.out.println("try to login 6");
+        	System.out.println("try to login 7");
+        	System.out.println("try to login8 ");
+        	System.out.println("try to login 9");
+        }
+    }
   }
 }
